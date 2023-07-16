df395fa69e515634f1e99c1c75b0b1ea2632df61
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2023-07-16 03:02:01 -0400

    Completed - adding put async await functionality

--- api/server.js
+++ api/server.js
@@ -5,6 +5,31 @@ const User = require("./users/model");
 const server = express();
 server.use(express.json());
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const updatedUserById = await User.findById(req.params.id);
+    const user = req.body;
+    if (!updatedUserById) {
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
+    } else if (!user.name || !user.bio) {
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
+    } else {
+      const updatedUser = await User.update(req.params.id, req.body);
+      res.status(200).json(updatedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be modified",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
 server.delete("/api/users/:id", async (req, res) => {
   try {
     const possibleUser = await User.findById(req.params.id);
